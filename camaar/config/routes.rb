54d3e9afef8e8816b9f1c4287589ff5fc10e02c0
jpantonov <jpantonow@gmail.com>
2024-05-22 22:26:05 -0300

    blur password

--- camaar/config/routes.rb
+++ camaar/config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   get 'home/index'
-  devise_for :users
+  # devise_for :users
   resources :users
+
+  devise_for :users,
+  :path => 'users',
+  :path_names => {
+    :sign_in => 'login',
+    :sign_up => 'register',
+    :sign_out => 'logout',
+    :password => 'recover-password',
+    :confirmation => 'verification'
+  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
